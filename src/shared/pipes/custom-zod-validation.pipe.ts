dce33a53240401e48b98b01ec8f377588ad35e8a
longdo <dolong2310@gmail.com>
2026-02-09 20:45:06 +0700

    chore: update ZodError import to version 4 in HTTP exception filter and enhance custom Zod validation pipe type

--- src/shared/pipes/custom-zod-validation.pipe.ts
+++ src/shared/pipes/custom-zod-validation.pipe.ts
@@ -1,8 +1,8 @@
 import { BadRequestException } from '@nestjs/common';
-import { createZodValidationPipe } from 'nestjs-zod';
+import { createZodValidationPipe, ZodValidationPipe } from 'nestjs-zod';
 import { ZodError } from 'zod';
 
-export const CustomZodValidationPipe = createZodValidationPipe({
+export const CustomZodValidationPipe: typeof ZodValidationPipe = createZodValidationPipe({
   // provide custom validation exception factory
   createValidationException: (error: ZodError) => {
     // TODO: Kiem tra neu body empty thi loi no return lai "undefined" => [{"code":"invalid_type","expected":"string","received":"undefined","path":[],"message":"Required"}]
