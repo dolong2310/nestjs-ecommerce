cfae44f3bb45c313b0e35e219e4db30e7ce53f12
longdo <dolong2310@gmail.com>
2026-02-09 16:06:18 +0700

    feat: add helmet middleware for enhanced security in NestJS application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import envConfig from '@/shared/config';
 import { WebsocketAdapter } from '@/websockets/websocket.adapter';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -23,6 +24,11 @@ async function bootstrap() {
 
   // Cấu hình phục vụ cho các proxy nginx hoặc apache để bảo vệ nestjs server từ việc bị tấn công DoS (Denial of Service) hoặc DDOS (Distributed Denial of Service)
   app.set('trust proxy', 'loopback'); // Trust requests from the loopback address
+
+  // Helmet là một middleware giúp bảo vệ ứng dụng Express/NestJS bằng cách tự động thiết lập các HTTP Header bảo mật quan trọng.
+  // Nói đơn giản, helmet giống như một "lớp áo giáp" bổ sung cho ứng dụng web, giúp giảm nguy cơ bị tấn công như XSS, clickjacking, lộ thông tin server, v.v.
+  // Việc này chủ yếu thông qua việc tự động thêm/chỉnh sửa các HTTP header mà trình duyệt và client sẽ dùng để tăng mức độ an toàn cho ứng dụng.
+  app.use(helmet());
 
   await app.listen(envConfig.PORT ?? 3000);
 }
