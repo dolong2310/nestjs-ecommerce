508abcd4257045a69e465d2995f2ffacc830e48a
longdo <dolong2310@gmail.com>
2026-02-28 20:23:30 +0700

    chore: update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,16 @@
 import { AppService } from '@/app.service';
-import { Controller } from '@nestjs/common';
+import { CURRENT_VERSION } from '@/shared/constants/version.constant';
+import { Public } from '@/shared/decorators/auth.decorator';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+@Controller({ version: CURRENT_VERSION })
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('hello')
+  @Public()
+  hello() {
+    console.log('hello world');
+    return 'Hello World!';
+  }
 }
